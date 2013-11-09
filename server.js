f0c2174a4537f13ff8a51159255c3e3d359a7b25
Moin Nadeem <moinnadeem@moinnadeem.com>
2013-11-08 20:51:15 -0600

    added barebones mongodb and facebook code.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// add VoteKO and that root thingy later
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('yotDA5W4DvTFZREf');
-
+var mongoose = require('mongoose');
 var express = require("express");
-var app = express();
+
 var isProduction = (process.env.NODE_ENV === 'production');
 var port = (isProduction ? 80 : 8000);
 
+var app = express();
 app.configure(function() {
     app.set('port', port);
+    app.set('views', __dirname + '/views');
     app.set("view options", {
         layout: false
     }); 
@@ -18,9 +21,44 @@ app.configure(function() {
 
 app.listen(port);
 console.log("Started listening on: " + port);
-// add VoteKO and that root thingy later
 
 app.get('/', function(req, res) {
-    res.send('It has begun.');
+    res.render('index.html');
 });
 
+// Mongo code, Daniel will have to help me out here:
+mongoose.connect('mongodb://moin.2013.nodeknockout.com/objective');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var findOrCreate = require('mongoose-findorcreate')
+  var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    facebookId: Number,
+    task: [{name:String, dueDate: Date, notes:String, URL:String, }]
+  })
+  userSchema.plugin(findOrCreate);
+  var User = mongoose.model('User', userSchema);
+});
+
+// Facebook Login Code
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: 544341338985998,
+    clientSecret: 'f73d3cc3000e547091bed93608c1dfa8',
+    callbackURL: "http://www.moin.2013.nodeknockout.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
